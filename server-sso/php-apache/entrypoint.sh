fdcc6d9b88b651673cd01ed89418f324f7db3406
hoangnv <thienpt@s-connect.net>
2024-09-12 17:34:24 +0700

    build

---
+++ server-sso/php-apache/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dừng script nếu có lỗi
+set -e
+
+# Cài đặt các gói Composer
+#composer install
+#composer update
+# Cài đặt các gói npm
+#npm install
+
+# Chạy lệnh build (nếu cần)
+#npm run build
+
+# Khởi động Apache hoặc bất kỳ dịch vụ nào bạn cần
+apache2-foreground
